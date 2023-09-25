ed6d13579bd0079ca253f959438f75623da843f4
Aniket Shandilya <aniket62058@gmail.com>
2023-09-25 22:51:10 +0530

    Added Data.java

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static Student []list=new Student[51];
+    static
+    {
+		//This is dummy dataset
+        list[0]=new Student("*********01","*********22","Aniket Shandilya","CSE");
+        list[1]=new Student("*********02","*********35","Abhimanyu Sharma","CSE");
+
+    }
+
+    public static String [] user={"------","++++","====="};
+    public static String []password={"(((((","))))","===="};
+}
